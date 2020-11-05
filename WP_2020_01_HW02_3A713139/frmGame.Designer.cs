3e34cd3625bea9d6e6bc8bc1920fb7e3e97874f9
peter2330 <a0970690012@gmail.com>
2020-11-05 18:28:16 +0800

    one

--- WP_2020_01_HW02_3A713139/frmGame.Designer.cs
+++ WP_2020_01_HW02_3A713139/frmGame.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WP_2020_01_HW02_3A713139
 {
-    partial class Form1
+    partial class frmGame
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -30,37 +30,43 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picResult = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(477, 51);
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(439, 42);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(187, 56);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "gogogo";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(154, 56);
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(131, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.Size = new System.Drawing.Size(94, 47);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "歷程";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // pictureBox1
+            // picResult
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(391, 127);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(277, 255);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.picResult.Image = global::WP_2020_01_HW02_3A713139.Properties.Resources._003;
+            this.picResult.Location = new System.Drawing.Point(393, 118);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(277, 255);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picResult.TabIndex = 2;
+            this.picResult.TabStop = false;
+            this.picResult.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // richTextBox1
             // 
@@ -69,19 +75,21 @@
             this.richTextBox1.Size = new System.Drawing.Size(195, 252);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
-            // Form1
+            // frmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picResult);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "frmGame";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,7 +99,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picResult;
         private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
---
+++ WP_2020_01_HW02_3A713139/frmGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_3A713139.Properties;
+
+namespace WP_2020_01_HW02_3A713139
+{
+    public partial class frmGame : Form
+    {
+        
+        List<Image> list = new List<Image>();
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._001);
+            list.Add(Resources._002);
+            list.Add(Resources._003);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = new Random().Next(3);//模擬隨機產生 一個值;
+            picResult.Image = list[index];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
